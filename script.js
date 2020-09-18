d24527028aea0ab64b8c249868cdf6538adf14c4
Danny Fan <fan.danny@gmail.com>
2020-09-18 09:18:59 +0800

    df

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 
+
+//get URL 
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 var lang = urlParams.get('lang')
 
 // function of language switcher
-function selectLang(){
+function selectLang() {
   var href = new URL(window.location.href);
-var searchParams = new URLSearchParams(window.location.search);
+  var searchParams = new URLSearchParams(window.location.search);
 
-  if(lang=="en"){
-    
-    href.searchParams.set('lang','tc');
-    
-    
-  }else{
-    
-    href.searchParams.set('lang','en');
-    
-    
+  if (lang == "en") {
+
+    href.searchParams.set('lang', 'tc');
+
+
+  } else {
+
+    href.searchParams.set('lang', 'en');
+
+
   }
-  window.open(href.toString(),"_self");
+  window.open(href.toString(), "_self");
   var newParams = searchParams.toString();
- 
+
 }
 
 
 
 
-// set anthe text
 
 
 
