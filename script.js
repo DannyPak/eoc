7ad0877431b047055263d3ab0bcaf49df9896f57
Danny Fan <fan.danny@gmail.com>
2020-09-28 15:56:54 +0800

    df

--- script.js
+++ script.js
@@ -27,6 +27,22 @@ function selectLang() {
 
 }
 
+//back to top button
+var btn = $('#back-button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
 
 
 
