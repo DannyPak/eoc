0ec6e769f175d221fc64851cd5eab0bfb69e07ea
DannyPak <fan.danny@gmail.com>
2020-10-11 16:01:35 +0800

    df

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 
 
+
+
 //get URL 
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 var lang = urlParams.get('lang')
 
+
 // function of language switcher
 function selectLang() {
   var href = new URL(window.location.href);
   var searchParams = new URLSearchParams(window.location.search);
+
+  
+      //Do stuff
+  
+
+
 
   if (lang == "en") {
 
@@ -50,11 +59,11 @@ function breadcumb(menu, lang) {
   document.getElementById("level-0").setAttribute("href", menu["m1"].link + "?lang=" + lang);
   if (lang != "tc") {
     document.getElementById("level-0").innerHTML = menu["m1"].en;
-    document.getElementById("level-1").innerHTML = document.getElementById("title-en").innerHTML;
+    document.getElementById("level-1").innerHTML = document.getElementById("page-title").innerHTML;
   }else{
 
     document.getElementById("level-0").innerHTML = menu["m1"].tc;
-    document.getElementById("level-1").innerHTML = document.getElementById("title-tc").innerHTML;
+    document.getElementById("level-1").innerHTML = document.getElementById("page-title").innerHTML;
 
 
   }
